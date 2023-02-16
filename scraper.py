89682db1272217b7cac006abd7d264c4d416a86b
Molly <mojeanmac@gmail.com>
2023-02-16 10:32:46 -0800

    title + selftext

--- scraper.py
+++ scraper.py
@@ -1,13 +1,14 @@
 import praw
-#import pandas as pd
+import pandas as pd
 
-#https://www.reddit.com/prefs/apps
 reddit = praw.Reddit(client_id="dh55NhdB6Swr9fPZLfBkxw",      # your client id
                      client_secret="Lk2HgqcAENPI5a--643zQi5BcdCN6Q",  #your client secret
                      user_agent="my user agent") #user agent name
 
-subreddit = reddit.subreddit("robloxparents")
-hot = subreddit.hot(limit=5)
+robloxparents = reddit.subreddit("robloxparents")
+top = robloxparents.top(limit=5)
 
-for submission in hot:
+#https://praw.readthedocs.io/en/stable/code_overview/models/submission.html#praw.models.Submission
+for submission in top:
     print(submission.title)
+    print(submission.selftext)
