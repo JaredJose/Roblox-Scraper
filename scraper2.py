3bc619d5c5175f9a9f272f7cc5ae60a6478ac065
JaredJose <jjose@ucsd.edu>
2023-02-17 08:50:41 -0800

    created new file for scraping comments

---
+++ scraper2.py
@@ -0,0 +1,16 @@
+import praw
+import pandas as pd
+
+reddit = praw.Reddit(client_id="dh55NhdB6Swr9fPZLfBkxw",      # your client id
+                     client_secret="Lk2HgqcAENPI5a--643zQi5BcdCN6Q",  #your client secret
+                     user_agent="my user agent") #user agent name
+
+robloxparents = reddit.subreddit("robloxparents")
+top = robloxparents.top(limit=5)
+
+#https://praw.readthedocs.io/en/stable/code_overview/models/submission.html#praw.models.Submission
+for submission in top:
+    print("Title:\t" + submission.title)
+    print("Text:\t" + submission.selftext)
+    for comment in submission.comments:
+        print("Comment:\t" + comment.body)
