08eeafded8e3312cfcc4b130f4c9663b48b88e55
Molly <mojeanmac@gmail.com>
2023-03-03 09:49:32 -0800

    combining posts+comments in one csv

--- r-p_All.py
+++ r-p_All.py
@@ -8,42 +8,62 @@ reddit = praw.Reddit(client_id="dh55NhdB6Swr9fPZLfBkxw",      # your client id
                      user_agent="my user agent") #user agent name
 
 robloxparents = reddit.subreddit("robloxparents")
-top = robloxparents.top(limit=1000)
+top = robloxparents.top(limit=10)
 
+type_list = []
 url_list = [] 
 author_list = []
+title_list = []
 date_list = []
 timestamp_list = []
 score_list = []
-comment_list = []
+text_list = []
 comment_id_list = []
 parent_id_list = []
+i = 0
+
 for post in top:
+    print(i)
+    i += 1
     if post is None:
-        #print("NONE TYPE NONE TYPE")
+        print("deleted post")
         continue
+    
+    type_list.append('Post')
+    url_list.append(post.url)
+    title_list.append(post.title)
+    if post.author is None:
+        print("deleted post author")
+        author_list.append('N/A')
+    else:
+        author_list.append(post.author.name)
+    date_list.append(datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d'))
+    timestamp_list.append(post.created_utc)
+    score_list.append(post.score)
+    text_list.append(post.selftext)
+    comment_id_list.append('N/A')
+    parent_id_list.append('N/A')
+    
     post.comments.replace_more(limit=None)
     comments = post.comments.list()
     for comment in comments:
         if comment is None:
-            #print("NONE TYPE NONE TYPE")
+            print("deleted comment")
             continue
-        #print('we balling')
-        url_list.append(post.url)   
+        type_list.append('comment')
+        url_list.append(post.url)
+        title_list.append('N/A')  
         if comment.author is None:
-            print("NOPE")
+            print("deleted comment author")
             author_list.append('N/A')
         else:
             author_list.append(comment.author.name)
         date_list.append(datetime.utcfromtimestamp(comment.created_utc).strftime('%Y-%m-%d'))
         timestamp_list.append(comment.created_utc)
         score_list.append(comment.score)
-        comment_list.append(comment.body)
+        text_list.append(comment.body)
         comment_id_list.append(comment.id)
-        # if isinstance(comment.parent(), Submission):
-        #           parent_id_list.append('N/A')
-        # else:
-        #     parent_id_list.append(comment.parent().id)
+
         par = comment.parent()
         if type(par) is praw.models.Submission:
             parent_id_list.append('N/A')
@@ -51,5 +71,5 @@ for post in top:
             parent_id_list.append(par.id)
 
 
-df = pd.DataFrame({'url': url_list, 'author': author_list, 'date': date_list, 'timestamp': timestamp_list, 'score': score_list, 'comment': comment_list, 'comment_id': comment_id_list, 'parent_id': parent_id_list})
-df.to_csv('data/robloxParentsComments.csv', index=False)
+df = pd.DataFrame({'type': type_list, 'url': url_list, 'title': title_list, 'author': author_list, 'date': date_list, 'timestamp': timestamp_list, 'score': score_list, 'text': text_list, 'comment_id': comment_id_list, 'parent_id': parent_id_list})
+df.to_csv('data/ALLrobloxParents.csv', index=False)
